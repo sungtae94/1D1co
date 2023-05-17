367aef1e0bdd7ca00cfabf74ab39f46a94eacfd0
sungtae <20573963@naver.com>
2023-05-17 23:24:41 +0900

    ------------------------- Spring Boot 세팅 Commit -------------------------

---
+++ DDO/src/main/java/DD/DDO/DdoApplication.java
@@ -0,0 +1,13 @@
+package DD.DDO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdoApplication.class, args);
+	}
+
+}
